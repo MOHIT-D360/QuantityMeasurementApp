e23beed4218afd9fbd357dd31973b0a0b47f37b2
MOHIT-D360 <mohitnsp786@gmail.com>
2026-02-23 15:04:04 +0530

    [Mohit] : added Inches inner class in Quantity Measurement App

--- src/main/java/com/QuantityMeasurementApp.java
+++ src/main/java/com/QuantityMeasurementApp.java
@@ -27,4 +27,46 @@ public class QuantityMeasurementApp {
 		}
 		
 	}
+	
+	public static class Inches {
+		private final double value;
+		
+		public Inches(double value) {
+			this.value  = value;
+		}
+		
+		@Override
+		public boolean equals(Object obj) {
+			if(this == obj) return true;
+			if(obj == null || getClass() != obj.getClass()) return false;
+			
+			Inches other = (Inches) obj;
+			return Double.compare(this.value, other.value) ==0;
+		}
+		
+		@Override 
+		public int hashCode() {
+			return Double.hashCode(value);
+		}
+		
+		
+	}
+	
+	public static boolean compareFeet(double v1,double v2) {
+		Feet f1 = new Feet(v1);
+		Feet f2 = new Feet(v2);
+		return f1.equals(f2);
+	}
+	
+	public static boolean compareInches(double v1,double v2) {
+		Inches i1 = new Inches(v1);
+		Inches i2 = new Inches(v2);
+		return i1.equals(i2);
+	}
+	// main
+	public static void main(String[] args) {
+		System.out.println("1.0 inch vs 1.0 inch = "+ compareInches(1.0,1.0) );
+		
+		System.out.println("1.0 ft vs 1.0 ft = "+ compareFeet(1.0,1.0));
+	}
 }
