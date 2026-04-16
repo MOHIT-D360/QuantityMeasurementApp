105089733bd5eb0af3b5d3ed5e9a89c82cbbdc86
MOHIT-D360 <mohitnsp786@gmail.com>
2026-04-16 13:46:53 +0530

    [Mohit] solving errors

--- src/test/java/com/app/quantitymeasurement/controller/QuantityMeasurementControllerTest.java
+++ src/test/java/com/app/quantitymeasurement/controller/QuantityMeasurementControllerTest.java
@@ -4,10 +4,10 @@ import org.mockito.Mockito;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
-import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.bean.override.mockito.*;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -24,7 +24,7 @@ public class QuantityMeasurementControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockitoBean
+    @MockBean
     private IQuantityMeasurementService service;
 
     private ObjectMapper objectMapper = new ObjectMapper();
